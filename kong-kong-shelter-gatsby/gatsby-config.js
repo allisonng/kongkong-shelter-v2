b6b69754b413dafdb69e9402c841768b467cc128
Allison Ng <aln.ng3@gmail.com>
2023-04-16 00:55:48 -0700

    Gatsby setup with basic config.

---
+++ kong-kong-shelter-gatsby/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `Kong Kong Shelter`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-netlify", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, "gatsby-plugin-netlify"]
+};
